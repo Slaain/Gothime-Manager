c5b06742beb284168570180d7c23be7b8ce21860
Milo <charon.odessa@gmail.com>
2024-10-08 13:51:34 +0200

    requete GET userID et username & email marche

--- time_manager_api/lib/time_manager_api/accounts.ex
+++ time_manager_api/lib/time_manager_api/accounts.ex
@@ -5,17 +5,10 @@ defmodule TimeManagerApi.Accounts do
 
   import Ecto.Query, warn: false
   alias TimeManagerApi.Repo
-
   alias TimeManagerApi.Accounts.User
 
   @doc """
   Returns the list of users.
-
-  ## Examples
-
-      iex> list_users()
-      [%User{}, ...]
-
   """
   def list_users do
     Repo.all(User)
@@ -23,31 +16,32 @@ defmodule TimeManagerApi.Accounts do
 
   @doc """
   Gets a single user.
-
-  Raises `Ecto.NoResultsError` if the User does not exist.
-
-  ## Examples
-
-      iex> get_user!(123)
-      %User{}
-
-      iex> get_user!(456)
-      ** (Ecto.NoResultsError)
-
   """
   def get_user!(id), do: Repo.get!(User, id)
 
   @doc """
+  Gets users by email and username.
+  """
+  def get_users_by_email_and_username(email, username) do
+    Repo.all(from u in User, where: u.email == ^email and u.username == ^username)
+  end
+
+  @doc """
+  Gets users by email.
+  """
+  def get_users_by_email(email) do
+    Repo.all(from u in User, where: u.email == ^email)
+  end
+
+  @doc """
+  Gets users by username.
+  """
+  def get_users_by_username(username) do
+    Repo.all(from u in User, where: u.username == ^username)
+  end
+
+  @doc """
   Creates a user.
-
-  ## Examples
-
-      iex> create_user(%{field: value})
-      {:ok, %User{}}
-
-      iex> create_user(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
   """
   def create_user(attrs \\ %{}) do
     %User{}
@@ -57,15 +51,6 @@ defmodule TimeManagerApi.Accounts do
 
   @doc """
   Updates a user.
-
-  ## Examples
-
-      iex> update_user(user, %{field: new_value})
-      {:ok, %User{}}
-
-      iex> update_user(user, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
   """
   def update_user(%User{} = user, attrs) do
     user
@@ -75,15 +60,6 @@ defmodule TimeManagerApi.Accounts do
 
   @doc """
   Deletes a user.
-
-  ## Examples
-
-      iex> delete_user(user)
-      {:ok, %User{}}
-
-      iex> delete_user(user)
-      {:error, %Ecto.Changeset{}}
-
   """
   def delete_user(%User{} = user) do
     Repo.delete(user)
@@ -91,12 +67,6 @@ defmodule TimeManagerApi.Accounts do
 
   @doc """
   Returns an `%Ecto.Changeset{}` for tracking user changes.
-
-  ## Examples
-
-      iex> change_user(user)
-      %Ecto.Changeset{data: %User{}}
-
   """
   def change_user(%User{} = user, attrs \\ %{}) do
     User.changeset(user, attrs)
--- time_manager_api/lib/time_manager_api_web/controllers/user_controller.ex
+++ time_manager_api/lib/time_manager_api_web/controllers/user_controller.ex
@@ -6,11 +6,32 @@ defmodule TimeManagerApiWeb.UserController do
 
   action_fallback TimeManagerApiWeb.FallbackController
 
+  @doc """
+  Handles GET requests for users, with optional filters for email and username.
+  """
+  def index(conn, %{"email" => email, "username" => username}) do
+    users = Accounts.get_users_by_email_and_username(email, username)
+    render(conn, :index, users: users)
+  end
+
+  def index(conn, %{"email" => email}) do
+    users = Accounts.get_users_by_email(email)
+    render(conn, :index, users: users)
+  end
+
+  def index(conn, %{"username" => username}) do
+    users = Accounts.get_users_by_username(username)
+    render(conn, :index, users: users)
+  end
+
   def index(conn, _params) do
     users = Accounts.list_users()
     render(conn, :index, users: users)
   end
 
+  @doc """
+  Handles POST requests to create a new user.
+  """
   def create(conn, %{"user" => user_params}) do
     with {:ok, %User{} = user} <- Accounts.create_user(user_params) do
       conn
@@ -20,11 +41,17 @@ defmodule TimeManagerApiWeb.UserController do
     end
   end
 
+  @doc """
+  Handles GET requests to fetch a specific user by ID.
+  """
   def show(conn, %{"id" => id}) do
     user = Accounts.get_user!(id)
     render(conn, :show, user: user)
   end
 
+  @doc """
+  Handles PUT requests to update an existing user.
+  """
   def update(conn, %{"id" => id, "user" => user_params}) do
     user = Accounts.get_user!(id)
 
@@ -33,6 +60,9 @@ defmodule TimeManagerApiWeb.UserController do
     end
   end
 
+  @doc """
+  Handles DELETE requests to delete a specific user.
+  """
   def delete(conn, %{"id" => id}) do
     user = Accounts.get_user!(id)
 
