6dd7ba31179c1724930ec48090d70556dab67a3b
WassimSss <tojiikan@gmail.com>
2024-10-09 16:43:46 +0200

    add total_time

--- time_manager_api/lib/time_manager_api_web/controllers/working_time_controller.ex
+++ time_manager_api/lib/time_manager_api_web/controllers/working_time_controller.ex
@@ -1,9 +1,10 @@
 defmodule TimeManagerApiWeb.WorkingTimeController do
   use TimeManagerApiWeb, :controller
-
+  require Logger
   alias TimeManagerApi.Timesheet
   alias TimeManagerApi.Timesheet.WorkingTime
   alias TimeManagerApi.Repo
+
 
   action_fallback TimeManagerApiWeb.FallbackController
 
@@ -21,15 +22,59 @@ defmodule TimeManagerApiWeb.WorkingTimeController do
 
   # POST: /api/workingtime/:userID
   def create(conn, %{"userID" => user_id, "working_time" => working_time_params}) do
-    working_time_params = Map.put_new(working_time_params, "end", nil)  # Si end n'est pas fourni, il reste nil
+    # Si "start" et "end" sont fournis, on calcule la différence en minutes
+    total_time = case {working_time_params["start"], working_time_params["end"]} do
+      {start_str, end_str} when not is_nil(start_str) and not is_nil(end_str) ->
+        # Log des valeurs reçues
+        Logger.debug("Start time: #{start_str}")
+        Logger.debug("End time: #{end_str}")
+
+        # Conversion en NaiveDateTime
+        start = NaiveDateTime.from_iso8601!(start_str)
+        end_time = NaiveDateTime.from_iso8601!(end_str)
+
+        # Log des objets NaiveDateTime créés
+        Logger.debug("Parsed start time: #{inspect(start)}")
+        Logger.debug("Parsed end time: #{inspect(end_time)}")
+
+        # Calcul de la différence en minutes
+        time_diff = NaiveDateTime.diff(end_time, start, :minute)
+
+        # Log de la différence de temps calculée
+        Logger.debug("Time difference in minutes: #{time_diff}")
+
+        time_diff
+
+      _ ->
+        # Log si l'un des champs est manquant
+        Logger.error("Missing start or end time in the parameters")
+        nil
+    end
+
+    # Ajouter ou mettre à jour le champ total_time dans les paramètres
+    Logger.debug("total_time: #{inspect(total_time)}")
+
+    working_time_params = Map.put(working_time_params, "total_time", total_time)
+    Logger.debug("working_time_params: #{inspect(working_time_params)}")
+
+
+
+    # Créer un changeset avec les paramètres modifiés
     changeset = WorkingTime.changeset(%WorkingTime{}, Map.put(working_time_params, "user_id", user_id))
+
+    Logger.debug("Changeset: #{inspect(changeset)}")
 
     case Repo.insert(changeset) do
       {:ok, working_time} ->
         conn
         |> put_status(:created)
         |> put_resp_header("location", ~p"/api/workingtime/#{user_id}/#{working_time.id}")
-        |> render("show.json", working_time: working_time)
+        |> json(%{
+          id: working_time.id,
+          start: working_time.start,
+          end: working_time.end,
+          total_time: working_time.total_time
+        })
 
       {:error, changeset} ->
         conn
@@ -37,6 +82,7 @@ defmodule TimeManagerApiWeb.WorkingTimeController do
         |> render(TimeManagerApiWeb.ChangesetView, "error.json", changeset: changeset)
     end
   end
+
 
 
 
--- time_manager_api/priv/repo/migrations/20241007134704_create_workingtimes.exs
+++ time_manager_api/priv/repo/migrations/20241007134704_create_workingtimes.exs
@@ -6,7 +6,7 @@ defmodule TimeManagerApi.Repo.Migrations.CreateWorkingtimes do
       add :start, :naive_datetime
       add :end, :naive_datetime
       add :user_id, references(:users, on_delete: :nothing)
-
+      add :total_time, :integer
       timestamps(type: :utc_datetime)
     end
 
