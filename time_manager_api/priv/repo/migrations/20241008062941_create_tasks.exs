bbc91407d1d7143b1d1f03ba24c1c171e0a6f862
WassimSss <tojiikan@gmail.com>
2024-10-10 11:10:44 +0200

    fix migration

---
+++ time_manager_api/priv/repo/migrations/20241008062941_create_tasks.exs
@@ -0,0 +1,15 @@
+defmodule TimeManagerApi.Repo.Migrations.CreateTasks do
+  use Ecto.Migration
+  # git push
+  def change do
+    create table(:tasks) do
+      add :title, :string
+      add :description, :string
+      add :status, :string
+      add :user_id, references(:users, on_delete: :delete_all)
+
+      timestamps()
+    end
+  end
+end
+#op
