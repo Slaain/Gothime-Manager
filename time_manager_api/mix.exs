8e493fd5fe28bcec7f0461711e036bef719da3a5
Nafissatou Sylla <nafissatou.sylla98@gmail.com>
2024-10-15 10:04:29 +0200

    change elixir version

--- time_manager_api/mix.exs
+++ time_manager_api/mix.exs
@@ -5,7 +5,7 @@ defmodule TimeManagerApi.MixProject do
     [
       app: :time_manager_api,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.17.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
