5a0bbdaea70e0aa0bd9d4e70bae1441fec8c2d29
Nafissatou Sylla <nafissatou.sylla98@gmail.com>
2024-10-21 23:09:28 +0200

    docker

--- time_manager_api/config/dev.exs
+++ time_manager_api/config/dev.exs
@@ -6,6 +6,7 @@ config :time_manager_api, TimeManagerApi.Repo,
   password: "postgres",
   hostname: "db",
   database: "time_manager_api_dev",
+  port: "5432",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
