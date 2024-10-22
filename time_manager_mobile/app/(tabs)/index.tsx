b3a7f5773a03e4c5c7d58c30be0bba7fb4e38437
Ali <idris1390@gmail.com>
2024-10-22 11:35:13 +0200

    finition page d'accueil + async storage

--- time_manager_mobile/app/(tabs)/index.tsx
+++ time_manager_mobile/app/(tabs)/index.tsx
@@ -1,70 +1,159 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Image, TextInput, KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import axios from 'axios';
+import { storeToken } from '../../components/asyncStorage'; // Chemin relatif vers asyncStorage.tsx
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
-}
+    const [email, SetEmail] = useState('');
+    const [password, SetPassword] = useState('');
 
+    const handleLogin = async () => {
+        try {
+            const response = await axios.post('http://localhost:8000/', {
+                email,
+                password,
+            });
+            const { token, user } = response.data;
+            await storeToken(token);
+            Alert.alert('Login Successful', 'You are now logged in!');
+            console.log(user);
+        } catch (error) {
+            console.error("Error during connection", error);
+            Alert.alert('Login Failed', 'Please check your credentials.');
+        }
+    };
+
+    return (
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <LinearGradient
+                colors={['#3e3e3e', '#E3B75B', '#3e3e3e']}
+                style={styles.backgroundContainer}
+            >
+                <KeyboardAvoidingView
+                    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                    style={styles.backgroundContainer}
+                >
+                    <View style={styles.titleContainer}>
+                        <Text style={styles.titleText}>Be Where Gotham Needs You.</Text>
+                        <Text style={styles.subtitleText}>
+                            Your role is key in Gotham's pulse. Log in to track your hours, view your schedule, and be a part of the city's heartbeat. The streets are counting on you.
+                        </Text>
+                    </View>
+
+                    <Image
+                        source={require('../../assets/images/logo.png')}
+                        style={styles.logo}
+                        resizeMode="contain"
+                    />
+
+                    <View style={styles.spacer} />
+
+                    <View style={styles.formContainer}>
+                        <TextInput
+                            placeholder="Email"
+                            value={email}
+                            onChangeText={SetEmail}
+                            style={styles.input}
+                            placeholderTextColor="#F3F3F3"
+                            keyboardType="email-address"
+                            autoCapitalize="none"
+                        />
+                        <TextInput
+                            placeholder="Password"
+                            value={password}
+                            onChangeText={SetPassword}
+                            style={styles.input}
+                            placeholderTextColor="#F3F3F3"
+                            secureTextEntry
+                        />
+                        <TouchableOpacity style={styles.signInButton} onPress={handleLogin}>
+                            <Text style={styles.buttonText}>Sign In</Text>
+                        </TouchableOpacity>
+                        <Text style={styles.forgetPasswordText}>
+                            Forget Password?
+                        </Text>
+                    </View>
+                </KeyboardAvoidingView>
+            </LinearGradient>
+        </TouchableWithoutFeedback>
+    );
+}
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+    backgroundContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+    },
+    titleContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 30,
+        marginTop: 50, // Adjust as needed
+    },
+    titleText: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: '#F3F3F3', // White or near-white
+        textAlign: 'center',
+        marginBottom: 15,
+        fontFamily: 'BatmanForeverRegular', // Assuming you loaded a custom font
+    },
+    subtitleText: {
+        fontSize: 16,
+        color: '#F3F3F3',
+        textAlign: 'center',
+        marginHorizontal: 20,
+        lineHeight: 24,
+        fontFamily: 'Arial',
+        marginTop: 3, // Adjust as needed
+    },
+    logo: {
+        width: 200,  // Adjust width based on your design
+        height: 150, // Adjust height based on your design
+        marginBottom: 30,
+        marginTop: 50, // Adds spacing between the logo and buttons
+    },
+    spacer: {
+        flex: 1, // Fills space between title and buttons to push buttons to the bottom
+    },
+    formContainer: {
+        width: '100%',
+        alignItems: 'center',
+        marginBottom: 40,
+    },
+    input: {
+        width: 250,
+        height: 50,
+        backgroundColor: '#333333',
+        borderRadius: 5,
+        paddingHorizontal: 15,
+        color: '#fff',
+        fontSize: 16,
+        marginBottom: 20,
+        borderWidth: 1,
+        borderColor: '#E3B75B',
+    },
+    signInButton: {
+        paddingHorizontal: 30,
+        paddingVertical: 10,
+        backgroundColor: '#333333', // Dark button background
+        borderRadius: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 20, // Space between button and "Forget Password?" link
+        borderWidth: 1,
+        borderColor: '#E3B75B', // Matching the yellow tone for the border
+    },
+    buttonText: {
+        color: '#FFF',
+        fontSize: 16,
+    },
+    forgetPasswordText: {
+        color: '#E3B75B', // Matching the yellow tone for the link
+        fontSize: 14,
+        textAlign: 'center',
+        textDecorationLine: 'underline', // To underline the text like a hyperlink
+    },
 });
