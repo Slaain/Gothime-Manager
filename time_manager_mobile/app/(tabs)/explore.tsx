357a54c450d65efb32f0b0b56d21637c530ac871
Ali <idris1390@gmail.com>
2024-10-22 17:10:33 +0200

    ok frontend wt

--- time_manager_mobile/app/(tabs)/explore.tsx
+++ time_manager_mobile/app/(tabs)/explore.tsx
@@ -1,26 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, ActivityIndicator, FlatList } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator, ImageBackground, Dimensions, ScrollView } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import axios from 'axios';
 import { useLocalSearchParams } from 'expo-router';
 import moment from 'moment';
 
 export default function GothamNeedsYouScreen() {
-    const { userId, userName } = useLocalSearchParams();  // Utilise userName pour récupérer le nom de l'utilisateur
+    const { userId, userName } = useLocalSearchParams();
     const [workingTimes, setWorkingTimes] = useState([]);
     const [loading, setLoading] = useState(true);
 
-    // Function to fetch working times
+    // Fetch working times
     const getWorkingTimes = async () => {
         try {
-            console.log("Fetching working times for userId:", userId); // Debugging log
             if (!userId) {
                 console.error("Error: userId is undefined or null");
                 setLoading(false);
                 return;
             }
             const response = await axios.get(`http://10.79.216.151:4000/api/workingtimes/${userId}`);
-            console.log('Working times fetched:', response.data.data[3]);
             setWorkingTimes(response.data.data);
             setLoading(false);
         } catch (error) {
@@ -30,58 +28,73 @@ export default function GothamNeedsYouScreen() {
     };
 
     useEffect(() => {
-        getWorkingTimes(); // Fetch working times on component mount
+        getWorkingTimes();
     }, []);
 
-    // Fonction pour formater les dates dans un format lisible
-    const formatDateTime = (dateTime) => {
-        return moment(dateTime).format('MMMM Do YYYY, h:mm:ss a');
-    };
+    // Format date and time
+    const formatDateTime = (dateTime) => moment(dateTime).format('DD/MM/YYYY');
+    const formatTime = (dateTime) => moment(dateTime).format('HH:mm');
 
     return (
-        <LinearGradient
-            colors={['#3e3e3e', '#E3B75B', '#3e3e3e']}
-            style={styles.backgroundContainer}
+        <ImageBackground
+            source={require('../../assets/images/batground.png')}
+            style={styles.backgroundImage}
+            resizeMode="cover"
         >
-            <View style={styles.contentContainer}>
-                <Text style={styles.titleText}>Gotham, need you</Text>
-                {/* Remplace l'affichage de l'email par celui du nom d'utilisateur */}
-                <Text style={styles.userNameText}>{userName}</Text>
+            <ScrollView contentContainerStyle={styles.scrollContainer} showsVerticalScrollIndicator={false}>
+                <LinearGradient
+                    colors={['#00000080', '#00000080', '#3e3e3e']}
+                    style={styles.overlay}
+                >
+                    <View style={styles.contentContainer}>
+                        <Text style={styles.titleText}>Gotham, need you</Text>
+                        <Text style={styles.userNameText}>{userName}</Text>
+                        <Text style={styles.userNameText}>Your working times</Text>
 
-                {loading ? (
-                    <ActivityIndicator size="large" color="#E3B75B" />
-                ) : (
-                    <FlatList
-                        data={workingTimes}
-                        keyExtractor={(item) => item.id.toString()}
-                        renderItem={({ item }) => (
-                            <View style={styles.workingTimeItem}>
-                                <Text style={styles.workingTimeText}>
-                                    Début : {formatDateTime(item.start)}, Fin : {formatDateTime(item.end)}
-                                </Text>
-                                <Text style={styles.workingTimeText}>
-                                    Temps total : {item.total_time ? `${item.total_time} heures` : 'Non disponible'}
-                                </Text>
-                            </View>
+                        {loading ? (
+                            <ActivityIndicator size="large" color="#E3B75B" />
+                        ) : (
+                            workingTimes.map((item) => (
+                                <View key={item.id} style={styles.workingTimeItem}>
+                                    <Text style={styles.workingTimeDateText}>
+                                        {formatDateTime(item.start)}
+                                    </Text>
+                                    <Text style={styles.workingTimeGroupText}>Groupe A</Text>
+                                    <Text style={styles.workingTimeHourText}>
+                                        {formatTime(item.start)} - {formatTime(item.end)}
+                                    </Text>
+                                    <Text style={styles.workingTimeHourText}>
+                                        {item.total_time ? `${item.total_time} heures` : 'Non disponible'}
+                                    </Text>
+                                </View>
+                            ))
                         )}
-                    />
-                )}
-            </View>
-        </LinearGradient>
+                    </View>
+                </LinearGradient>
+            </ScrollView>
+        </ImageBackground>
     );
 }
 
 const styles = StyleSheet.create({
-    backgroundContainer: {
+    backgroundImage: {
         flex: 1,
-        justifyContent: 'center',
+        width: Dimensions.get('window').width,
+        height: '100%',  // Prendre toute la hauteur de l'écran
+    },
+    scrollContainer: {
+        paddingBottom: 100,  // Ajustement du padding pour éviter les coupures
+    },
+    overlay: {
+        flex: 1,
+        justifyContent: 'flex-start',
         alignItems: 'center',
         padding: 20,
     },
     contentContainer: {
-        justifyContent: 'center',
+        paddingTop: 80,  // Pour éviter que le titre soit coupé
+        width: '100%',
         alignItems: 'center',
-        marginTop: 70,
     },
     titleText: {
         fontSize: 32,
@@ -91,7 +104,6 @@ const styles = StyleSheet.create({
         marginBottom: 15,
         fontFamily: 'BatmanForeverRegular',
     },
-    // Mise à jour pour l'affichage du user_name
     userNameText: {
         fontSize: 24,
         color: '#E3B75B',
@@ -100,14 +112,32 @@ const styles = StyleSheet.create({
         fontFamily: 'Arial',
     },
     workingTimeItem: {
-        backgroundColor: '#333',
-        padding: 10,
-        marginVertical: 5,
-        borderRadius: 5,
-        width: '100%',
+        backgroundColor: 'rgba(255, 255, 255, 0.1)',
+        padding: 25,
+        marginVertical: 15,
+        borderRadius: 15,
+        borderWidth: 1,
+        borderColor: '#E3B75B',
+        width: Dimensions.get('window').width * 0.9,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.3,
+        shadowRadius: 10,
+        elevation: 6,
     },
-    workingTimeText: {
+    workingTimeDateText: {
+        color: '#E3B75B',
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    workingTimeGroupText: {
+        color: '#FFF',
+        fontSize: 18,
+        marginTop: 10,
+    },
+    workingTimeHourText: {
         color: '#FFF',
         fontSize: 16,
+        marginTop: 5,
     },
 });
