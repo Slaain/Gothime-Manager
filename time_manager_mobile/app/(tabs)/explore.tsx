4ce54d5ca79637392fe7028034f7a930927f0b60
WassimSss <tojiikan@gmail.com>
2024-10-23 11:20:07 +0200

    op

--- time_manager_mobile/app/(tabs)/explore.tsx
+++ time_manager_mobile/app/(tabs)/explore.tsx
@@ -1,6 +1,7 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { StyleSheet, Image, Platform, Text } from 'react-native';
 import Home from '@/components/Home';
+import Test from '@/components/Test';
 
 export default function TabTwoScreen() {
   return (
