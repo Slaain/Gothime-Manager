b8f53450362b29b774f9b1557363b861b4830c0a
WassimSss <tojiikan@gmail.com>
2024-10-23 10:18:03 +0200

    op

--- time_manager_mobile/components/Home.tsx
+++ time_manager_mobile/components/Home.tsx
@@ -1,19 +1,32 @@
-import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text as DefaultText, View, Dimensions, ScrollView } from 'react-native';
 
 export default function HelloWave() {
+  const { width: screenWidth, height: windowHeight } = Dimensions.get('window');
+
+  const colors = {
+    "primary": "#FDCB12"
+  }
+  const Text = (props) => <DefaultText {...props} style={[props.style, { fontFamily: 'Montserrat' }]} />;
+  const TextBold = (props) => <DefaultText {...props} style={[props.style, { fontFamily: 'MontserratBold' }]} />;
+  const TextOrbitron = (props) => <DefaultText {...props} style={[props.style, { fontFamily: 'Orbitron' }]} />;
+  const TextOrbitronBold = (props) => <DefaultText {...props} style={[props.style, { fontFamily: 'OrbitronBold' }]} />;
 
   return (
     <ImageBackground source={require('../assets/images/batground.png')} style={style.container}>
+      <ScrollView
+        contentContainerStyle={[{ paddingTop: windowHeight * 0.1, paddingBottom: (windowHeight * 0.1) + 20, backgroundColor: "rgba(0,0,0,0.6)", width: screenWidth, flex: 1, paddingHorizontal: 20 }]}
 
-      <Text style={{ color: 'white', fontSize: 30, fontWeight: 'bold', marginBottom: 30 }}>Gotham needs you, <Text style={{
-        color: 'yellow'
-      }}>Paul</Text></Text>
-      <View style={style.glassmorphism}>
-        <Text style={{ color: 'yellow', fontSize: 30, fontWeight: 'bold' }}>Groupe A</Text>
-        <Text style={{ color: 'white', fontSize: 20 }}>08:00 - 12:00</Text>
+      >
+        <TextOrbitronBold style={{ color: 'white', fontSize: 25, marginBottom: 60 }}>Gotham needs you, <TextOrbitronBold style={{
+          color: colors.primary
+        }}>Paul</TextOrbitronBold></TextOrbitronBold>
+        <View style={[style.glassmorphism, { width: "100%", padding: 15 }]}>
+          <TextOrbitronBold style={{ color: colors.primary, fontSize: 20, marginBottom: 30 }}>Groupe A</TextOrbitronBold>
+          <Text style={{ marginBottom: 10 }}>Paul Durive - Pierre Deloup - Idris Ali - Ali Idris - Sam Sam</Text>
+          <Text style={{}}>08:00 - 12:00</Text>
 
-      </View>
-
+        </View>
+      </ScrollView>
     </ImageBackground >
   );
 }
@@ -22,15 +35,16 @@ const style = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
+    width: '100%',
 
   },
   glassmorphism: {
     backgroundColor: 'rgba(255, 255, 255, 0.4)',
     width: 230,
     height: 230,
-    justifyContent: 'center',
-    alignItems: 'center',
+    // justifyContent: 'center',
+    // alignItems: 'center',
     borderRadius: 15,
     borderWidth: 1,
     borderColor: 'rgba(255, 255, 255, 1)',
@@ -43,5 +57,8 @@ const style = StyleSheet.create({
     shadowRadius: 10,
     elevation: 10,
     marginBottom: 30,
+    filter: 'blur(10)',
+
+
   },
 });
