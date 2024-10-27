0b5ca0155db29620426ce9a7520ec21aee42605a
Milo <charon.odessa@gmail.com>
2024-10-27 22:23:12 +0100

    ajout logo dashboard

--- time_manager_frontend/src/components/Sidebar.vue
+++ time_manager_frontend/src/components/Sidebar.vue
@@ -1,7 +1,7 @@
 <template>
     <aside class="text-white bg-gray-900 sidebar">
       <div class="flex items-center justify-center py-6 logo">
-        <img src="./assets/avatar.png" alt="Vue Logo" class="w-12 h-12" />
+        <img src="/public/logo.ico" alt="Vue Logo" class="w-12 h-12" />
       </div>
       <nav class="flex flex-col space-y-4 text-center nav">
         <router-link to="/admin" class="p-2 bg-yellow-500 rounded-md hover:bg-yellow-700">
@@ -12,9 +12,6 @@
         </router-link>
         <router-link to="/users" class="p-2 bg-gray-700 rounded-md hover:bg-gray-800">
           Users
-        </router-link>
-        <router-link to="/settings" class="p-2 bg-gray-700 rounded-md hover:bg-gray-800">
-          Settings
         </router-link>
         <!-- Bouton pour les Organisations -->
         <router-link to="/organisations" class="p-2 bg-gray-700 rounded-md hover:bg-gray-800">
